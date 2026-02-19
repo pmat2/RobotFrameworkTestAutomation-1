8fd9cf97814b26913b74a812338d8538aff7acc7
Your Name <you@example.com>
2026-02-19 22:10:58 +0100

    fixed bug with wrong variable usage

--- embedded/app.c
+++ embedded/app.c
@@ -3,6 +3,7 @@
 #include "hal_temperature.h"
 
 static int alarm_active;
+static int temp;
 
 void app_process(void) {
     alarm_active = hal_get_temperature() > 80;
@@ -12,8 +13,8 @@ int main(void) {
     char cmd[64];
 
     while (fgets(cmd, sizeof(cmd), stdin)) {
-        if (sscanf(cmd, "SET_TEMP %d", &alarm_active)) {
-            hal_set_mock_temperature(alarm_active);
+        if (sscanf(cmd, "SET_TEMP %d", &temp)) {
+            hal_set_mock_temperature(temp);
             app_process();
             printf("OK\n");
         } else if (strncmp(cmd, "GET_ALARM", 9) == 0) {
